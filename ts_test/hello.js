a1285ad965d36dd1cffbff7c8bb84bc4c6d0bccd
nhan2211 <220057xa@yse-c.net>
2023-11-28 11:47:48 +0900

    update

--- ts_test/hello.js
+++ ts_test/hello.js
@@ -5,22 +5,18 @@ var message = "Hello!!!!";
 if (message) {
     console.log(message);
 }
-
 // Type Inference （型推論）
 var id = 1;
 var email = "test@test.com";
 var isActive = true;
 var none;
-
 // Type Definition （型定義）
-var id:number = 1;
-var email:string = "test@test.com";
-var isActive:boolean = true;
-var none:any;
-
+var id = 1;
+var email = "test@test.com";
+var isActive = true;
+var none;
 // none = "YSE";
 // none = 5;
-
 console.log(id, typeof id);
 console.log(email, typeof email);
 console.log(isActive, typeof isActive);
